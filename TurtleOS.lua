886b62af19509f9ef58c71cac028b0c3bca87d62
goldminer127 <67210677+goldminer127@users.noreply.github.com>
2021-09-06 20:13:37 -0400

    Testing update

--- TurtleOS.lua
+++ TurtleOS.lua
@@ -1,18 +1,24 @@
+--OS Info
+Version = "BETA 0.1.0"
+
+
+
 --General Functions
 
 --Splits string with specified character
-function Split(string1, string2)
+function Split(string1, char1)
     local stringInsert = ""
     local splitString = {}
-    for char in string1:gmatch "." do
-        if char == " " then
+    for char in string1:gmatch"." do
+        if char == char1 then
             table.insert(splitString, stringInsert)
             stringInsert = ""
         else
             stringInsert = stringInsert .. char
         end
     end
-    return stringInsert
+    table.insert(splitString, stringInsert)
+    return splitString
 end
 
 --Formats messages to be 30 chars long.
@@ -29,40 +35,136 @@ function Format(string1, string2)
     return string1 .. spaces .. string2
 end
 
-
 --OS Functions
 
-function Turtle(input)
-    if input[0] == nil then
-        print("Use 'turtle help' to view available commands. Use 'turtle help <command> to read about a specific command.")
-    elseif input[0] == "get" then
-
+--Check if OS Version file is updated.
+function CheckForUpdate()
+    local gitVersion = pcall(fs.open("Version","r"))
+    if gitVersion == true then
+        gitVersion.delete("Version")
+        gitVersion.close()
     end
+    local download = http.get("https://raw.githubusercontent.com/goldminer127/TurtleOS/main/Version")
+    local turtleOS = download.readAll()
+    download.close()
+    local file = fs.open("Version","w")
+    file.write(turtleOS)
+    file.close()
+    
+    --Returns true if there is no update. Returns false if there is an update
+    local versionFile = fs.open("Version", "r")
+    local isUpToDate = versionFile.readAll() == Version
+    versionFile.close()
+    return isUpToDate
 end
 
+--Store initial version or store new version after update
+function UpdateVersion()
+    local osVersion = pcall(fs.open("OSVersion","r"))
+    if osVersion == true then
+        osVersion.delete("OSVersion")
+        osVersion.close()
+    end
+    local versionFile = fs.open("OSVersion","w")
+    versionFile.write(Version)
+    versionFile.close()
+    --pcall(osVersion.close())
+end
 
+--Update TurtleOS
+function Update()
+    local turtleOS = fs.open("TurtleOS","r")
+    local osDownload = http.get("https://raw.githubusercontent.com/goldminer127/TurtleOS/main/TurtleOS.lua")
+    local downloadFile = osDownload.readAll()
+    osDownload.close()
+
+    --Delete old version
+    turtleOS.delete("TurtleOS")
+    turtleOS.close()
+
+    --Install new version
+    turtleOS = fs.open("TurtleOS","w")
+    turtleOS.write(downloadFile)
+    turtleOS.close()
+end
+
+--Restarts the OS **NOT THE TURTLE**
+function Restart()
+    shell.run("clear")
+    shell.run("TurtleOS")
+end
 
 --Turtle Commands
-function Command(command)
+
+function Listener()
     while true do
-        local input = Split(read())
-        local prefix = input[0]
-        if prefix == nil then
-            print("\n")
+        term.write("> ")
+        local input = Split(read(),' ')
+        local prefix = input[1]
+        table.remove(input, 1)
         --Default turtle commands
-        elseif input[0] == "turtle" then
+        if prefix == "turtle" then
+            Turtle(input)
         end
     end
 end
---Main
-print(Format("Hello", "[ OK ]"))
-print(Format("Loading Framework", "[ OK ]"))
-local file = http.get(https://github.com/goldminer127/TurtleOS/blob/main/README.md)
-        local handle = file.readAll()
-        file.close()
 
-        local newFile = fs.open("README.md", "w")
-        newFile.write(handle)
-        file.close()
+
+function Turtle(input)
+    if input[1] == nil then
+        print("Use 'turtle help' to view available commands. Use 'turtle help <command> to read about a specific command.")
+    elseif input[1] == "update" then
+        --True = no update, False = new update available
+        if CheckForUpdate() == true then
+            print("OS is up to date")
+        else
+            local newVersionFile = fs.open("Version","r")
+            local newVersionNum = newVersionFile.readAll()
+            newVersionFile.close()
+            print("OS", newVersionNum, "is available.\nUpdate will require a restart.\nReady to update? (y/n)")
+            local response = read()
+            if response == "y" then
+                Update()
+                Restart()
+            else
+                print("Update postponed")
+            end
+        end
+    elseif input[1] == "restart" then
+        Restart()
+    end
+end
+
+--Startup stuff
+
+function Startup()
+    UpdateVersion()
+    --Display arbutrary messages for fun. It indicated nothing
+    --Loading messages
+    print(Format("Loading OS", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Loading IP", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Loading emotions", "[ FAILED ]"))
+    print("Starting systems...\n")
+    --Starting messages
+    sleep(2)
+    print(Format("Starting kernal", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Starting modules", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Starting interface", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Starting other services", "[ OK ]"))
+    sleep(0.25)
+    print(Format("Starting emotions", "[ FAILED ]"))
+    print("\n\nWelcome to TurtleOS. Loading...")
+    sleep(5)
+    shell.run("clear")
+    Listener()
+end
+
+--Main
+Startup()
 --shell.run("pastebin","get","1F1cR1LH","turtle")
 --shell.run("pastebin get 1F1cR1LH turtle")
