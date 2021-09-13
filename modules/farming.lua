6cad80e40a38c6943d594206fb1075079248ee9f
goldminer127 <67210677+goldminer127@users.noreply.github.com>
2021-09-13 13:42:35 -0400

    Fixed Farming

--- modules/farming.lua
+++ modules/farming.lua
@@ -1,20 +1,23 @@
+--Table used to hold all functions
+local farming = {}
+
 --Module info
 Version = "BETA 0.1.0"
 IsStable = false
 
 --Returns string (version)
-function GetVersion()
-    print("test")
+function farming.PrintVersion()
+    print(Version)
 end
 
 --Returns bool
-function GetStability()
-    return IsStable
+function farming.PrintStability()
+    print(IsStable)
 end
 
 --Module Functions
 
-function TurtleForward()
+function farming.TurtleForward()
     local canmove = turtle.forward()
     turtle.suck()
     if canmove == false then
@@ -25,7 +28,7 @@ function TurtleForward()
     end
 end
 
-function FarmSquare(x)
+function farming.FarmSquare(x)
     for row = 1, x do
         for col = 1, x do
             TurtleForward()
@@ -42,10 +45,11 @@ function FarmSquare(x)
     end
 end
 
-function Interpreter(input)
+function farming.Interpreter(input)
     if input[1] == "-version" then
-        print("Farming",GetVersion())
+        print("Farming",PrintVersion())
     --elseif input[1].count == 2 then
         
     end
 end
+return farming
